3605126ce1cb8287d3c0f414ac43f10e8324ffd8
d_sunbg <172272984@qq.com>
2022-04-28 20:21:57 +0800

    init

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import {Button} from "antd";
+
+import logo from '../../logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
@@ -7,7 +11,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Button onClick={()=>console.log("lzrnb")}>
+            猜猜我是谁，lzrup
+          </Button>
+          给李兆荣一拳！
         </p>
         <a
           className="App-link"
