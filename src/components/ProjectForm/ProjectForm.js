af17f2c5eff26c59e236665bc3a04ffe15715104
d_sunbg <172272984@qq.com>
2022-05-04 22:59:28 +0800

    temp

--- src/components/ProjectForm/ProjectForm.js
+++ src/components/ProjectForm/ProjectForm.js
@@ -2,7 +2,8 @@ import {FormWidth, FormMaxHeight} from "../../data/constants.js";
 import React from "react";
 
 import "./ProjectForm.css"
-import {Empty} from "antd";
+import {Empty, Input, Upload, message, Button} from "antd";
+import { UploadOutlined } from '@ant-design/icons';
 
 /**
  *
@@ -12,12 +13,14 @@ import {Empty} from "antd";
  */
 function TypeOfText (props){
   return (<div className="m-pf-text">
-      <div className="p-point">{props.point.id}</div>
+      <div className="u-point">{props.point.id}</div>
       <div>
         <label className="m-pf-text-label">
           请设置备注
           <br/>
-          <input type="text"/>
+          <Input type="text" onInput={props.oninput} value={
+            props.data[props.point.id]
+          }/>
         </label>
       </div>
   </div>
@@ -27,20 +30,19 @@ function TypeOfText (props){
 function TypeOfImage (props){
   return (
     <div className="m-pf-image">
-      <div className="p-point">{props.point.id}</div>
+      <div className="u-point">{props.point.id}</div>
       <div>
-        <label className="m-pf-image">
+        <label className="m-pf-image-label">
           请设置备注
-          <input type="image" accept="image/gif,image/jpeg,image/jpg,image/png" alt='404'/>
-          {
-
-          }
+          <br/>
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
         </label>
       </div>
     </div>
   )
 }
-
 
 export default class ProjectForm extends React.Component{
 
@@ -48,14 +50,36 @@ export default class ProjectForm extends React.Component{
     super(props);
     this.props = props
     this.state = {
-      project:null
+      project:null,
+      data:[]
     }
   }
 
   updateProject(project){
-    this.setState({project})
+
+    if (this.state.project === null){
+      this.setState({
+        project:project,
+        data: []
+      })
+      return
+    }
+
+    const { points } = this.state.project
+
+    if (points.length - 1 === project.points.length){}
+
+    //   (this.state)=>{
+    //   return {
+    //     project: project
+    //   }
+    // }
   }
 
+  updateStateData = (data, i)=>{
+    this.state.data[i] = data
+    console.log(this.state)
+  }
 
   formList(){
     /**
@@ -65,19 +89,27 @@ export default class ProjectForm extends React.Component{
     if (!project){
       return
     }
+
     const {points} = project
     let list = []
     for (let i = 0; i <points.length; i++){
-      list.push(<TypeOfText point = {points[i]}/>)
+      this.state.data.push('')
+      if (points[i].type === 1){
+        list.push(
+          <TypeOfText point = {points[i]}
+                      oninput = {(data)=>{this.updateStateData(data.target.value, i)}}
+                      data = {this.state.data}
+          />
+        )
+      }else if (points[i].type === 2){
+        list.push(<TypeOfImage point = {points[i]}/>)
+      }
     }
     return list
   }
 
-
-
   render() {
     const {project} = this.state
-    console.log(project)
     return (
       <>
         {
@@ -87,16 +119,20 @@ export default class ProjectForm extends React.Component{
             请打开或新建项目
           </div>
         }
-
         {
-
           project &&
           <div className="pf-editor">
-            {this.formList()}
+            <div>
+              {this.formList()}
+            </div>
+            <div>
+              <button className="u-pf-preview" id='pfPreview'>预 览</button>
+              <button className="u-pf-save" id='pfSave'>保 存</button>
+            </div>
+
           </div>
         }
       </>
-
     )
   }
 }
